f34c7d08eb41c624ca2a27905f5e3470b22ab433
Tif Slama <tif.slama@gmail.com>
2014-10-03 15:57:26 -0700

    creation of files

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("hello friend");
+
+var markX = document.getElementsByClassName("top left")
+	
+	document.getElementsByClassName("box")addEventListener("click", markX);
+
+// markX.innerHTML = "X"
+
+// 1 on click, add X to innerHTML, then put that box in playerX's array, and check if they won
+// 2 on click, add O to innerHTML, then put that box in playerO's array, and check if they won
+
+
+// var markO = function () {
+// 	var square = document.querySelectorAll("")
+// }
+
+// // document.querySelector().addEventListener("click", )
+
+// var players = [playerX, playerO];
+
+// var playerX = {
+
+// }
+
+// var playerO = {
+
+// }
+
+
+
+// var wins = [[.top, .top, .top] [.right, .right, .right]]
+// wins are (3 .top) 
+	// or (3 .middle) 
+	// or (3 .bottom) 
+	// or (3 .left) 
+	// or (3 .center) 
+	// or (3 .right) 
+	// or (.top&.left + .middle&.center + .bottom&.right)
+	// or (.top&.right + .middle&.center + .bottom&.left)
